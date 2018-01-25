bb03e0e075b7648ff5f68ac89139b33041dabb5a
cesandovalp <sandovalp.ce@gmail.com>
2018-01-25 13:07:54 -0500

    Documentation

--- R/Plot.R
+++ R/Plot.R
@@ -8,10 +8,8 @@
 #' @param width          The width of the image.
 #' @param height         The height of the image.
 #' @param set_palette    0 = default palette; 1 = grayscale; 2 = rich.colors.
-#' @return A matrix of black/white pixels.
+#' @return A matrix of pixels.
 #' @export
-#' @examples
-#' pixel_matrix = IFSPlot::RunChaosGame(5, .4, 100**3)
 RunIFS = function(transformation, translation, probability, iterations, width, height, set_palette = 0) {
   result = IFS(transformation, translation, probability, iterations, width, height)
   par(mar=c(0, 0, 0, 0))
