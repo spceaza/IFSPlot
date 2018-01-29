90ec688f185af969ef44f3d9533dd109810835b3
cesandovalp <sandovalp.ce@gmail.com>
2018-01-29 01:01:32 -0500

    3D plots

--- R/Plot.R
+++ R/Plot.R
@@ -1,26 +1,73 @@
-#' RunIFS
+#' RunIFS2D
 #'
 #' This function allows you to plot some fractals using the Iterated Function Systems theory.
 #' @param transformation A list of size n, the 2 x 2 transformation matrices.
-#' @param translation    A list of size n, the 2 x 2 translational vectors.
+#' @param translation    A list of size n, the 2 x 1 translational vectors.
 #' @param probability    A list of size n, the weighted probabilities.
 #' @param iterations     The number of iterations/points.
-#' @param width          The number of pixels for the image.
+#' @param pixels         The number of pixels for the 2D plot.
 #' @param set_palette    0 = default palette; 1 = grayscale; 2 = rich.colors.
-#' @return A matrix of pixels.
+#' @param filename       If given, saves the image as a png file in the current working directory.
+#' @return A 2D array of pixels.
 #' @export
-RunIFS = function(transformation, translation, probability, iterations, pixels, set_palette = 0) {
-  result = IFS(transformation, translation, probability, iterations, pixels)
+RunIFS2D = function(transformation, translation, probability, iterations, pixels, set_palette = 0, filename = "") {
+  result = IFS2D(transformation, translation, probability, iterations, pixels)
   par(mar=c(0, 0, 0, 0))
   if(set_palette == 0) palette("default")
   if(set_palette == 1) palette(gray(length(table(result$ImageMatrix)):0 / length(table(result$ImageMatrix))))
   if(set_palette == 2) palette(rev(rich.colors(length(table(result$ImageMatrix)))))
   if(set_palette == 0) {
     result$ImageMatrix[result$ImageMatrix > 1] = 1
-    image(result$ImageMatrix, useRaster=TRUE, axes=FALSE, col = 0:1)
+    if(filename != "") {
+      png(filename = filename, width = result$width, height = result$height)
+      image(result$ImageMatrix, useRaster=TRUE, axes=FALSE, col = 0:1)
+      dev.off()
+    }
   }
   else
-    image(result$ImageMatrix, useRaster=TRUE, axes=FALSE, col = 0:length(table(result$ImageMatrix)))
-    
+    if(filename != "") {
+      png(filename = filename, width = result$width, height = result$height)
+      image(result$ImageMatrix, useRaster=TRUE, axes=FALSE, col = 0:length(table(result$ImageMatrix)))
+      dev.off()
+    }
+
+  result
+}
+
+#' RunIFS3D
+#'
+#' This function allows you to plot some fractals using the Iterated Function Systems theory.
+#' @param transformation A list of size n, the 3 x 3 transformation matrices.
+#' @param translation    A list of size n, the 3 x 1 translational vectors.
+#' @param probability    A list of size n, the weighted probabilities.
+#' @param iterations     The number of iterations/points.
+#' @param pixels         The number of pixels for the 3D plot.
+#' @param width          Width of the window.
+#' @param height         Height of the window.
+#' @param set_palette    0 = default palette; 1 = grayscale; 2 = rich.colors.
+#' @return A 3D array of pixels.
+#' @export
+RunIFS3D = function(transformation, translation, probability, iterations, pixels, width, height, set_palette = 0) {
+  result = IFS3D(transformation, translation, probability, iterations, pixels)
+  xyz = which(result$ImageMatrix > 0, arr.ind=TRUE)
+
+  if(set_palette == 0) {
+    df = data.frame(x = xyz[, 1], y = xyz[, 2], z = xyz[, 3])
+    palette("default")
+    with(df, plot3d(x, y, z))
+  }
+  if(set_palette == 1) {
+    df = data.frame(x = xyz[,1], y = xyz[,2], z = xyz[,3], colorby = result$ImageMatrix[xyz])
+    palette(gray(length(table(result$ImageMatrix[xyz])):0 / length(table(result$ImageMatrix[xyz]))))
+    with(df, plot3d(x, y, z, col = colorby))
+  }
+  if(set_palette == 2) {
+    df = data.frame(x=xyz[,1], y=xyz[,2], z=xyz[,3], colorby=result$ImageMatrix[xyz])
+    palette(rev(rich.colors(length(table(result$ImageMatrix[xyz])))))
+    with(df, plot3d(x, y, z, col = colorby))
+  }
+
+  par3d(windowRect = c(0, 0, width, height))
+
   result
 }
--- src/IFS.cpp
+++ src/IFS.cpp
@@ -1,7 +1,10 @@
-#include <Rcpp.h>
+#define ARMA_64BIT_WORD 1
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::plugins(cpp11)]]
 #include <vector>
 #include <math.h>
 #include <limits>
+#include <RcppArmadillo.h>
 #include "Matrix.hpp"
 
 using namespace Rcpp;
@@ -20,14 +23,8 @@ std::vector<double> ToVector(const NumericVector& v)
   return std::vector<double>(v.begin(), v.end());
 }
 
-template<typename... Ts>
-void test(Ts... args)
-{
-  std::vector<int> size = std::vector<int>({args...});
-}
-
 // [[Rcpp::export]]
-List IFS(const List& transformation, const List& translation, const std::vector<int>& probability, const int& iterations, const int pixels)
+List IFS2D(const List& transformation, const List& translation, const std::vector<int>& probability, const int& iterations, const int pixels)
 {
   std::vector<sp::Matrix<double>>  _transformation;
   std::vector<sp::Matrix<double>>  _translation;
@@ -71,7 +68,7 @@ List IFS(const List& transformation, const List& translation, const std::vector<
   h = (max_y - min_y) * p;
 
   NumericMatrix result(w, h);
-  
+
   for(int i = 0; i < iterations; ++i)
   {
     x[i] = (x[i] - min_x) * (w - 1)  / (max_x - min_x);
@@ -84,5 +81,77 @@ List IFS(const List& transformation, const List& translation, const std::vector<
   return List::create(Named("ImageMatrix") = result,
                       Named("width")       = w,
                       Named("height")      = h);
+
+}
+
+// [[Rcpp::export]]
+List IFS3D(const List& transformation, const List& translation, const std::vector<int>& probability, const int& iterations, const int pixels)
+{
+  std::vector<sp::Matrix<double>>  _transformation;
+  std::vector<sp::Matrix<double>>  _translation;
+  std::vector<int>                 rules_probability;
+  sp::Matrix<double>               point(std::vector<double>({0, 0, 0}));
+  std::vector<double>              x(iterations);
+  std::vector<double>              y(iterations);
+  std::vector<double>              z(iterations);
+  int index;
+  double min_x =  std::numeric_limits<double>::infinity();
+  double max_x = -std::numeric_limits<double>::infinity();
+  double min_y =  std::numeric_limits<double>::infinity();
+  double max_y = -std::numeric_limits<double>::infinity();
+  double min_z =  std::numeric_limits<double>::infinity();
+  double max_z = -std::numeric_limits<double>::infinity();
+  int w, h, l;
+
+  for (int i = 0; i < transformation.length(); ++i)
+    _transformation.push_back( ToMatrix( transformation(i) ) );
+
+  for (int i = 0; i < translation.length(); ++i)
+    _translation.push_back( sp::Matrix<double>( ToVector( translation(i) ) ) );
+
+  for( int i = 0; i < probability.size(); ++i)
+    for( int j = 0; j < probability[i]; ++j)
+      rules_probability.push_back(i);
+
+  for (int i = 0; i < iterations; i++)
+  {
+    index = rules_probability[rand() % 100];
+    point.Multiplication(_transformation[index], point);
+    point += _translation[index];
+    x[i] = point[0][0];
+    y[i] = point[1][0];
+    z[i] = point[2][0];
+    if(x[i] > max_x) max_x = x[i];
+    if(x[i] < min_x) min_x = x[i];
+    if(y[i] > max_y) max_y = y[i];
+    if(y[i] < min_y) min_y = y[i];
+    if(z[i] > max_z) max_z = z[i];
+    if(z[i] < min_z) min_z = z[i];
+  }
+
+  double p = (max_x - min_x);
+  p *= (max_y - min_y);
+  p *= (max_z - min_z);
+  p = pixels / p;
+  p = cbrt( p );
+
+  w = (max_x - min_x) * p;
+  h = (max_y - min_y) * p;
+  l = (max_z - min_z) * p;
+
+  arma::cube result((int)w, (int)h, (int)l);
+  result.zeros();
+
+  for(int i = 0; i < iterations; ++i)
+  {
+    x[i] = (x[i] - min_x) * (w - 1) / (max_x - min_x);
+    y[i] = (y[i] - min_y) * (h - 1) / (max_y - min_y);
+    z[i] = (z[i] - min_z) * (l - 1) / (max_z - min_z);
+
+    if(result(x[i], y[i], z[i]) < 100)
+      result(x[i], y[i], z[i]) += 1;
+  }
+
+  return List::create(Named("ImageMatrix") = result);
 
 }
