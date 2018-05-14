62c2edad0635768b3adeb396ddf42c3a0a2d1341
cesandovalp <sandovalp.ce@gmail.com>
2018-05-14 12:31:34 -0500

    OpenMP

--- src/IFS.cpp
+++ src/IFS.cpp
@@ -1,6 +1,10 @@
 #define ARMA_64BIT_WORD 1
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::plugins(cpp11)]]
+// [[Rcpp::plugins(openmp)]]
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 #include <vector>
 #include <math.h>
 #include <limits>
@@ -28,9 +32,17 @@ List IFS2D(const List& transformation, const List& translation, const std::vecto
 {
   std::vector<sp::Matrix<double>>  _transformation, _translation;
   std::vector<int>                 rules_probability;
-  sp::Matrix<double>               point(std::vector<double>({0, 0}));
-  std::vector<double>              x(iterations), y(iterations);
+  std::vector<sp::Matrix<double>>  point;
+  std::vector<std::vector<double>> x, y;
   int index;
+
+  for(int i = 0; i < 8; ++i)
+  {
+    x.push_back(std::vector<double>(iterations / 8));
+    y.push_back(std::vector<double>(iterations / 8));
+    point.push_back(std::vector<double>({0, 0}));
+  }
+
   int p_count = 0;
   double min_x, min_y, max_x, max_y;
   max_x = max_y = -std::numeric_limits<double>::infinity();
@@ -50,18 +62,20 @@ List IFS2D(const List& transformation, const List& translation, const std::vecto
       rules_probability.push_back(i);
   }
 
-  for (int i = 0; i < iterations; i++)
+  #pragma omp parallel for num_threads(8)
+  for (int i = 0; i < iterations / 8; i++)
   {
+    int tid = omp_get_thread_num();
     index = rules_probability[ R::runif( 0, p_count ) ];
-    point.Multiplication(_transformation[index], point);
-    point += _translation[index];
-    x[i] = abs(point[0][0]) > 10000 ? 0 : point[0][0];
-    y[i] = abs(point[1][0]) > 10000 ? 0 : point[1][0];
+    point[tid].Multiplication(_transformation[index], point[tid]);
+    point[tid] += _translation[index];
+    x[tid][i] = abs(point[tid][0][0]) > 10000 ? 0 : point[tid][0][0];
+    y[tid][i] = abs(point[tid][1][0]) > 10000 ? 0 : point[tid][1][0];
 
-    if(x[i] > max_x) max_x = x[i];
-    if(x[i] < min_x) min_x = x[i];
-    if(y[i] > max_y) max_y = y[i];
-    if(y[i] < min_y) min_y = y[i];
+    if(x[tid][i] > max_x) max_x = x[tid][i];
+    if(x[tid][i] < min_x) min_x = x[tid][i];
+    if(y[tid][i] > max_y) max_y = y[tid][i];
+    if(y[tid][i] < min_y) min_y = y[tid][i];
   }
 
   double p = sqrt(pixels/((max_x - min_x)*(max_y - min_y)));
@@ -71,14 +85,15 @@ List IFS2D(const List& transformation, const List& translation, const std::vecto
 
   NumericMatrix result(w, h);
 
-  for(int i = 0; i < iterations; ++i)
-  {
-    x[i] = (x[i] - min_x) * (w - 1)  / (max_x - min_x);
-    y[i] = (y[i] - min_y) * (h - 1) / (max_y - min_y);
+  for(int tid = 0; tid < 8; ++tid)
+    for(int i = 0; i < iterations / 8; ++i)
+    {
+      x[tid][i] = (x[tid][i] - min_x) * (w - 1) / (max_x - min_x);
+      y[tid][i] = (y[tid][i] - min_y) * (h - 1) / (max_y - min_y);
 
-    if(result(x[i], y[i]) < 100)
-      result(x[i], y[i]) += 1;
-  }
+      if(result(x[tid][i], y[tid][i]) < 100)
+        result(x[tid][i], y[tid][i]) += 1;
+    }
 
   return List::create(Named("ImageMatrix") = result);
 }
